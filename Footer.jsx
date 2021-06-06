5f19512ff358be0aaf4233e35bd43cb050ebfb39
vandhanna <84226953+vandhanna@users.noreply.github.com>
2021-06-07 00:25:43 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+function Footer(){
+
+  var curYear=new Date().getFullYear();
+  return(
+    <footer>
+        <p>Copyright @ {curYear}</p>
+    </footer>
+  );
+  
+}
+export default Footer;
---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note(){
+  return(
+    <div className="note">
+        <h1>javascript and react.js</h1>
+        <p> React is an open-source front-end JavaScript library for building user interfaces or UI components </p>
+    </div>
+  );
+  
+}
+ 
+export default Note;
